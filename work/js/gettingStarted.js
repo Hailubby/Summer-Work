e44112dea40c8eef2c4fd35aa0a346d9c1930369
Hailubby <hwan561@aucklanduni.ac.nz>
2016-12-19 10:34:00 +1300

    second set of code's animation. Introduces alias's

--- work/js/gettingStarted.js
+++ work/js/gettingStarted.js
@@ -172,4 +172,78 @@ $(document).ready(function () {
         }
 
     });
+
+
+    //page 2 code
+    $("#start-button2").click(function () {
+        $(".hide-all").hide();
+        $("#code2-line-6").animate({backgroundColor: 'transparent'}, 0);
+        b_line1();
+        $("#d2-rtn-addr-val").promise().done(function () {
+            b_line2();
+            $("#d2-name-val").promise().done(function () {
+                b_line3();
+                $("#d2-age-val").promise().done(function () {
+                    b_line4();
+                    $("#d2-name-alias").promise().done(function () {
+                        b_line5();
+                        $("#d2-age-alias").promise().done(function () {
+                            b_line6();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    //line 1 function
+    var b_line1 = function () {
+        $("#code2-line-1").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".table .stack-bottom").delay(200).animate({borderBottomWidth: '2px', borderBottomColor: 'black'},100);
+        $(".table .stack-top").delay(200).animate({borderTopWidth: '2px', borderTopColor: 'black'},100);
+        $(".table .stack-left").delay(200).animate({borderLeftWidth: '2px', borderLeftColor: 'black'},100);
+        $(".table .stack-right").delay(200).animate({borderRightWidth: '2px', borderRightColor: 'black'},100);
+        $("#d2-rtnval").delay(1200).show(400);
+        $("#d2-rtn-addr").delay(1200).show(400);
+        $("#d2-rtn-addr-val").delay(1600).show(400);
+
+    }
+
+    //line 2 function
+    var b_line2 = function () {
+        $("#code2-line-1").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code2-line-2").animate({backgroundColor: '#ffff00'}, 1000);
+        $("#d2-name").delay(1000).show(400);
+        $("#d2-name-val").delay(1400).show(400);
+    }
+
+    //line 3 function
+    var b_line3 = function () {
+        $("#code2-line-2").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code2-line-3").animate({backgroundColor: '#ffff00'}, 1000);
+        $("#d2-age").delay(1000).show(400);
+        $("#d2-age-val").delay(1400).show(400);
+    }
+
+    //line 4 function
+    var b_line4 = function () {
+        $("#code2-line-3").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code2-line-4").animate({backgroundColor: '#ffff00'}, 1000);
+        $("#d2-name-alias").delay(1000).show(400);
+    }
+
+    //line 5 function
+    var b_line5 = function () {
+        $("#code2-line-4").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code2-line-5").animate({backgroundColor: '#ffff00'}, 1000);
+        $("#d2-age-alias").delay(1000).show(400);
+    }
+
+    //line 6 function
+    var b_line6 = function () {
+        $("#code2-line-5").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code2-line-6").animate({backgroundColor: '#ffff00'}, 1000);
+        $("#d2-rtnval-val").delay(1000).show(400);
+    }
 });
+
