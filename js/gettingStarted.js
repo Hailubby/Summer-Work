f4ff7b5d1d13513fcb8a5215688b739326c7f81e
Hailubby <hwan561@aucklanduni.ac.nz>
2017-01-08 16:50:23 +1300

    Basic jquery for panel 3 code 3 implemented

--- js/gettingStarted.js
+++ js/gettingStarted.js
@@ -1208,6 +1208,268 @@ $(document).ready(function () {
 
     });
 
+    //panel 3 code 2
+    $("#start-button-3b").click(function () {
+        $(".d3b-hide-all").hide();
+        p3_c2_hideBorders();
+        $("#code3b-line-7").animate({backgroundColor: 'transparent'}, 0);
+        p3_c2_line1();
+        $("#d3b-rtn-addr-val").promise().done(function () {
+            p3_c2_line2();
+            $("#d3b-name-val").promise().done(function () {
+                p3_c2_line3()
+                $("#d3b-age-val").promise().done(function () {
+                    p3_c2_line4()
+                    $("#d3b-phone-val").promise().done(function () {
+                        p3_c2_line5();
+                        $("#d3b-name-val").promise().done(function () {
+                            p3_c2_line6();
+                            $("#d3b-phone-val").promise().done(function () {
+                                p3_c2_line7();
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    //p3 c2 line 1 function
+    var p3_c2_line1 = function () {
+        $("#code3b-line-1").animate({backgroundColor: '#ffff00'}, 600);
+        $(".d3b-link").removeClass("yellow");
+        $(".table .d3b-stack-bottom").delay(50).animate({borderBottomWidth: '2px', borderBottomColor: 'black'},30);
+        $(".table .d3b-stack-top").delay(50).animate({borderTopWidth: '2px', borderTopColor: 'black'},30);
+        $(".table .d3b-stack-left").delay(50).animate({borderLeftWidth: '2px', borderLeftColor: 'black'},30);
+        $(".table .d3b-stack-right").delay(50).animate({borderRightWidth: '2px', borderRightColor: 'black'},30);
+        $("#d3b-rtnval").delay(1200).show(400);
+        $("#d3b-rtn-addr").delay(1200).show(400);
+        $("#d3b-rtn-addr-val").delay(1600).show(400);
+    }
+
+    //p3 c2 line 2 function
+    var p3_c2_line2 = function () {
+        $("#code3b-line-1").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code3b-line-2").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".d3b-link").removeClass("yellow");
+        setTimeout(function(){
+            $("#d3b-string-top").addClass("stack-string-top");
+            $("#d3b-string-mid").addClass("stack-string-middle");
+            $("#d3b-string-bot").addClass("stack-string-bottom");
+        }, 800);
+        $("#d3b-name-val").html('"Bob"');
+        $("#d3b-name").delay(1000).show(400);
+        $("#d3b-name-val").delay(1400).show(400);
+    }
+
+    //p3 c2 line 3 function
+    var p3_c2_line3 = function () {
+        $("#code3b-line-2").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code3b-line-3").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".d3b-link").removeClass("yellow");
+        $("#d3b-age").delay(1000).show(400);
+        $("#d3b-age-val").delay(1400).show(400);
+    }
+
+    //p3 c2 line 4 function
+    var p3_c2_line4 = function () {
+        $("#code3b-line-3").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code3b-line-4").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".d3b-link").removeClass("yellow");
+        $("#d3b-phone").delay(1000).show(400);
+        $("#d3b-phone-val").delay(1400).show(400);
+    }
+
+    //p3 c2 line 5 function
+    var p3_c2_line5 = function () {
+        $("#code3b-line-4").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code3b-line-5").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".d3b-link").removeClass("yellow");
+        setTimeout(function(){
+            $("#d3b-name-val").html('"Bob Jones"');
+        }, 1000);
+        $("#d3b-name-val").delay(1100).animate({backgroundColor: 'transparent'}, 500);
+    }
+
+    //p3 c2 line 6 function
+    var p3_c2_line6 = function () {
+        $("#code3b-line-5").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code3b-line-6").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".d3a-link").removeClass("yellow");
+        setTimeout(function(){
+            $("#d3b-phone-val").html('2222222');
+        }, 1000);
+        $("#d3b-phone-val").delay(1100).animate({backgroundColor: 'transparent'}, 500);
+    }
+
+    //p3 c2 line 7 function
+    var p3_c2_line7 = function () {
+        $("#code3b-line-6").animate({backgroundColor: 'transparent'}, 1000);
+        $("#code3b-line-7").animate({backgroundColor: '#ffff00'}, 1000);
+        $(".d3b-link").removeClass("yellow");
+        $("#d3b-rtnval-val").delay(1000).show(400);
+    }
+
+
+    //unhighlight all lines
+    var p3_c2_unhighlight = function () {
+        $("#code3a-line-1").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-2").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-3").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-4").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-5").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-6").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-7").animate({backgroundColor: 'transparent'}, 0);
+    }
+
+    //show stack border
+    var p3_c2_showBorders = function () {
+        $(".table .d3b-stack-bottom").animate({borderBottomWidth: '2px', borderBottomColor: 'black'},0);
+        $(".table .d3b-stack-top").animate({borderTopWidth: '2px', borderTopColor: 'black'},0);
+        $(".table .d3b-stack-left").animate({borderLeftWidth: '2px', borderLeftColor: 'black'},0);
+        $(".table .d3b-stack-right").animate({borderRightWidth: '2px', borderRightColor: 'black'},0);
+    }
+
+    //hide stack borders
+    var p3_c2_hideBorders = function () {
+        $(".table .d3b-stack-bottom").animate({borderBottomWidth: '1px', borderBottomColor: '#dddddd'},0);
+        $(".table .d3b-stack-top").animate({borderTopWidth: '1px', borderTopColor: '#dddddd'},0);
+        $(".table .d3b-stack-left").animate({borderLeftWidth: '1px', borderLeftColor: '#dddddd'},0);
+        $(".table .d3b-stack-right").animate({borderRightWidth: '1px', borderRightColor: '#dddddd'},0);
+    }
+
+    //stepping function
+    var currentLine;
+    $("#code3a-line-1").click(function(){
+        $("#step-button-3a").removeClass("disabled");
+
+        $(".d3a-link").removeClass("yellow");
+        $(".d3a-link").removeClass("green");
+        $("#code3a-line-1").addClass("yellow");
+
+        $(".d3a-hide-all").hide();
+        p3_c1_hideBorders();
+        p3_c1_unhighlight();
+        currentLine = 1;
+    });
+
+    $("#code3a-line-2").click(function(){
+        $("#step-button-3a").removeClass("disabled");
+
+        $(".d3a-link").removeClass("yellow");
+        $(".d3a-link").removeClass("green");
+        $("#code3a-line-2").addClass("yellow");
+
+        p3_c1_showBorders();
+        $(".d3a-hide-all").hide();
+        $(".d3a-show1").show();
+        p3_c1_unhighlight();
+        currentLine = 2;
+    });
+
+    $("#code3a-line-3").click(function(){
+        $("#step-button-3a").removeClass("disabled");
+
+        $(".d3a-link").removeClass("yellow");
+        $(".d3a-link").removeClass("green");
+        $("#d3a-id-val").removeClass("green");
+        $("#d3a-id-val").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-3").addClass("green");
+
+        p3_c1_showBorders();
+        $(".d3a-hide-all").hide();
+        $("#d3a-id-val").html('1');
+        $(".d3a-show1").show();
+        $(".d3a-show2").show();
+        p3_c1_unhighlight();
+        currentLine = 3;
+    });
+
+    $("#code3a-line-4").click(function(){
+        $("#step-button-3a").removeClass("disabled");
+
+        $(".d3a-link").removeClass("yellow");
+        $(".d3a-link").removeClass("green");
+        $("#code3a-line-4").addClass("yellow");
+
+        p3_c1_showBorders();
+        $(".d3a-hide-all").hide();
+        $("#d3a-id-val").html('1');
+        $(".d3a-show1").show();
+        $(".d3a-show2").show();
+        $("#d3a-id-val").addClass("green");
+        p3_c1_unhighlight();
+        currentLine = 4;
+    });
+
+    $("#code3a-line-5").click(function(){
+        $("#step-button-3a").removeClass("disabled");
+
+        $(".d3a-link").removeClass("yellow");
+        $(".d3a-link").removeClass("green");
+        $("#d3a-id-val").removeClass("green");
+        $("#d3a-id-val").animate({backgroundColor: 'transparent'}, 0);
+        $("#code3a-line-5").addClass("green");
+
+        p3_c1_showBorders();
+        $(".d3a-hide-all").hide();
+        $("#d3a-id-val").html('2');
+        $(".d3a-show1").show();
+        $(".d3a-show2").show();
+        p3_c1_unhighlight();
+        currentLine = 5;
+    });
+
+    $("#code3a-line-6").click(function(){
+        $("#step-button-3a").removeClass("disabled");
+
+        $(".d3a-link").removeClass("yellow");
+        $(".d3a-link").removeClass("green");
+        $("#code3a-line-6").addClass("yellow");
+
+        p3_c1_showBorders();
+        $(".d3a-hide-all").hide();
+        $("#d3a-id-val").html('2');
+        $(".d3a-show1").show();
+        $(".d3a-show2").show();
+        $("#d3a-id-val").addClass("green");
+        p3_c1_unhighlight();
+        currentLine = 6;
+    });
+
+    $("#step-button-3a").click(function () {
+        if (currentLine == 1) {
+            //do line 1 things here
+            p3_c1_line1();
+            currentLine = 2;
+        }
+        else if (currentLine == 2) {
+            //do line 2 things here
+            p3_c1_line2();
+            currentLine = 3;
+        }
+        else if (currentLine == 3) {
+            //do line 3 things here
+            p3_c1_line3();
+            currentLine = 4;
+        }
+        else if (currentLine == 4) {
+            //do line 4 things here
+            p3_c1_line4();
+            currentLine = 5;
+        }
+        else if (currentLine == 5) {
+            p3_c1_line5();
+            currentLine = 6;
+        }
+        else if (currentLine == 6) {
+            p3_c1_line6();
+            $("#step-button-3a").addClass("disabled");
+            currentLine = 0;
+        }
+
+    });
+
     //page 2 code
     $("#start-button2").click(function () {
         $(".hide-all").hide();
